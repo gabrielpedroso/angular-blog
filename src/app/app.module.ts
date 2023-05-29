5fa861de1261fe6761f1a0da24e96d21b39f7dd8
gabrielpedroso <gabrielmacito@gmail.com>
2023-05-29 13:11:21 -0300

    html, css - components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PhotoCardComponent } from './components/photo-card/photo-card.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PhotoCardComponent,
+    BigCardComponent,
+    MenuBarComponent,
+    SmallCardComponent
   ],
   imports: [
     BrowserModule,
